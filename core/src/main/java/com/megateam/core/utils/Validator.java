45720f7a90326f523c145a517ddd72f44d0b5390
Vladislav Andreev <andreevvlad443@gmail.com>
2023-02-15 21:47:14 +0300

    validator class: validateInput method is throwing an exception if an input was incorrect

--- core/src/main/java/com/megateam/core/utils/Validator.java
+++ core/src/main/java/com/megateam/core/utils/Validator.java
@@ -1,11 +1,16 @@
 package com.megateam.core.utils;
 
+import com.megateam.core.exceptions.InvalidInputException;
+
 public class Validator
 {
 
-	public static boolean validateInput(String userInput)
+	public static boolean validateInput(String userInput) throws InvalidInputException
 	{
-		return !"".equals(userInput);
+		if ("".equals(userInput))
+			throw new InvalidInputException("Please enter non-empty command");
+
+		return true;
 	}
 
 }
