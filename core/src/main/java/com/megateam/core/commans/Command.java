c522ec9133f7f8907b95051998c3a223225eaa33
enzulode <enzulode@users.noreply.github.com>
2023-02-16 11:05:42 +0000

    Prettified Code!

--- core/src/main/java/com/megateam/core/commans/Command.java
+++ core/src/main/java/com/megateam/core/commans/Command.java
@@ -2,37 +2,32 @@ package com.megateam.core.commans;
 
 import com.megateam.core.utils.CommandSource;
 import com.megateam.core.utils.Printer;
-
 import java.io.Reader;
 import java.util.List;
 
-public abstract class Command
-{
+public abstract class Command {
+
 	private Reader reader;
 	private Printer printer;
 	protected CommandSource source;
 	protected String name;
 	protected String description;
 
-	protected Command(String name, String description, CommandSource source)
-	{
+	protected Command(String name, String description, CommandSource source) {
 		this.name = name;
 		this.description = description;
 		this.source = source;
 	}
 
-	public Printer getPrinter()
-	{
+	public Printer getPrinter() {
 		return printer;
 	}
 
-	public Reader getReader()
-	{
+	public Reader getReader() {
 		return reader;
 	}
 
-	public String help()
-	{
+	public String help() {
 		return String.format("%s - %s", name, description);
 	}
 
